9e0cd2246f9f51d3bbd1dda6df233aed76874497
heeray <hhxxdnp@163.com>
2017-11-05 00:18:03 +0800

    s1-p3 commit

--- main/section-1/practice-3.js
+++ main/section-1/practice-3.js
@@ -1,5 +1,10 @@
 'use strict';
 
 module.exports = function collectSameElements(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+ let arr=[];
+	collectionA.forEach((a)=>{
+		if(objectB.value.indexOf(a)!=-1)
+			arr.push(a);
+	});
+  return arr;
 }
