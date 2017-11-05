8ec67d40d0298e80da22c4ee961efce7a2eeff97
heeray <hhxxdnp@163.com>
2017-11-05 10:31:00 +0800

    s2-p1 commit

--- main/section-2/practice-3.js
+++ main/section-2/practice-3.js
@@ -1,5 +1,42 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+ let arr = [];
+	collection.forEach((a)=>{
+	//将collection中的每一个元素都以[‘元素’，数量]表示
+		let str=['item',0];
+		if(a.match('-')){
+			str[0]=a.split('-')[0];
+			str[1]=a.split('-')[1];
+		}
+		else if(a.match(':')){
+			str[0]=a.split(':')[0];
+			str[1]=a.split(':')[1];
+		}
+		else if(a.match(/\[/)){
+			str[0]=a.split(/\[/)[0];
+			str[1]=a.split(/\[/)[1];
+			if(str[1].match(/\]/)){
+				str[1]=str[1].split(/\]/)[0];
+			}
+		}
+		else{
+			str[0]=a;
+			str[1]=1;
+		}
+		str[1]=Number(str[1]);
+		
+    let p ;
+	p = arr.findIndex((b)=> {
+		return b.name == a[0];
+		});
+		if(p == -1){
+			
+				arr.push({name: str[0], summary: str[1]})
+		}
+		else{
+		arr[p].summary+=str[1];
+		}
+	});
+	return arr;
 }
