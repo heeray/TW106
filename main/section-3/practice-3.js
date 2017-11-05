c173c0e34513a5d68783d99da18cd58bddbe60d6
heeray <hhxxdnp@163.com>
2017-11-05 10:56:26 +0800

    s3-p3 commit

--- main/section-3/practice-3.js
+++ main/section-3/practice-3.js
@@ -1,5 +1,29 @@
 'use strict';
 
 module.exports = function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  let arr = [];
+	collectionA.forEach((a)=>{
+    let p ;
+	p = arr.findIndex((b)=> {
+		return b.key == a;
+		});
+		if(p == -1){
+			arr.push({key:a,count:1});
+		}
+		else{
+			arr[p].count++;
+		}
+	});
+	
+	objectB.value.forEach((a)=>{
+		arr.forEach((b)=>{
+			if(a==b.key)
+			{
+				let i=parseInt(b.count/3);
+				b.count-=i;
+			}
+		});
+	});
+
+	return arr;
 }
