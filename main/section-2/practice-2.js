594ca48ad5add553b89739551a943709617a8145
heeray <hhxxdnp@163.com>
2017-11-05 10:12:53 +0800

    s2-p2 commit

--- main/section-2/practice-2.js
+++ main/section-2/practice-2.js
@@ -1,5 +1,25 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  let arr = [];
+	collection.forEach((a)=>{
+    let p ;
+	p = arr.findIndex((b)=> {
+		return b.key == a;
+		});
+		if(p == -1){
+			if(a.match('-')){
+				let s;
+				s=a.split('-');
+				arr.push({key: s[0], count: Number(s[1])})
+			}
+			else{
+				arr.push({key:a,count:1});
+			}
+	}
+	else{
+		arr[p].count++;
+		}
+	});
+	return arr;
 }
